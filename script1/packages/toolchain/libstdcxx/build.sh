44fbf24469ceb00c622812d81eca53dd55f6238b
fcanatta5 <fernandocanatta5@gmail.com>
2025-12-09 12:36:34 -0300

    Update build.sh

--- script1/packages/toolchain/libstdcxx/build.sh
+++ script1/packages/toolchain/libstdcxx/build.sh
@@ -41,8 +41,8 @@ PKG_DEPENDS=(
 ###############################################################################
 # Mantemos o mesmo padrão dos outros pacotes:
 #   ${ADM_TARGET_ARCH:-x86_64}-lfs-linux-gnu
-
-PKG_TARGET_TRIPLET="${PKG_TARGET_TRIPLET:-${ADM_TARGET_ARCH:-x86_64}-lfs-linux-gnu}"
+# PKG_TARGET_TRIPLET="${PKG_TARGET_TRIPLET:-${ADM_TARGET_ARCH:-x86_64}-lfs-linux-gnu}"
+PKG_TARGET_TRIPLET="${PKG_TARGET_TRIPLET:-${ADM_TARGET_TRIPLET}}"
 
 ###############################################################################
 # Configure / Build / Install customizados
