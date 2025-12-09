4f1c190497980c20c6ad725cbcc5dc037e5d9ea4
fcanatta5 <fernandocanatta5@gmail.com>
2025-12-09 13:04:35 -0300

    Update build.sh

--- script1/packages/base/m4/build.sh
+++ script1/packages/base/m4/build.sh
@@ -1,46 +1,72 @@
 #!/usr/bin/env bash
-# Receita ADM para GNU M4 1.4.20
+# Receita ADM para GNU M4 1.4.20 (ferramenta de macro processamento)
+#
+# Este pacote é neutro em relação à libc:
+#   - Funciona tanto com glibc quanto com musl.
+#   - O que muda é somente o perfil do ADM (-P glibc / -P musl),
+#     que define TARGET_TRIPLET, ROOTFS, etc.
 
-# Metadados básicos
+###############################################################################
+# Metadados básicos do pacote
+###############################################################################
+
 PKG_NAME="m4"
 PKG_VERSION="1.4.20"
 
-# Fonte oficial + checksum
+# Tarball oficial do GNU M4
 PKG_URLS=(
-  "https://ftp.gnu.org/gnu/m4/m4-1.4.20.tar.xz"
-)
-PKG_SHA256S=(
-  "e236ea3a1ccf5f6c270b1c4bb60726f371fa49459a8eaaebc90b216b328daf2b"
+  "https://ftp.gnu.org/gnu/m4/m4-${PKG_VERSION}.tar.xz"
 )
 
-# Dependências de runtime/build (ajuste para a sua árvore de pacotes)
-# Ex.: se você tiver pacotes core/gcc, core/glibc, etc., liste aqui.
+# Opcional: SHA256 do tarball (recomendável preencher com o valor real)
+# PKG_SHA256S=(
+#   "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+# )
+
+# Dependências lógicas dentro do ADM:
+#   - GCC final em /usr (toolchain estável)
 PKG_DEPENDS=(
-  # "core/gcc"
-  # "core/glibc"
+  "toolchain/gcc"
 )
 
-# Opções extras de configure/make, se necessário
-# (o adm.sh já passa: --host, --build, --prefix=/usr, --sysconfdir=/etc, --localstatedir=/var)
-PKG_CONFIGURE_OPTS=(
-  "--disable-dependency-tracking"
-)
-
-# Se quiser algo extra de compilação:
-# PKG_CFLAGS_EXTRA=""
-# PKG_LDFLAGS_EXTRA=""
-
-# Caso precise alterar flags do make:
-# PKG_MAKE_OPTS=()
-# PKG_MAKE_INSTALL_OPTS=()
-
-# NOTA:
-#  - Não precisamos definir nenhuma função especial de build.
-#  - O adm.sh vai fazer automaticamente:
-#       ./configure (com argumentos acima)
-#       make -jN
-#       make DESTDIR=... install
+###############################################################################
+# Triplet alvo
+###############################################################################
+# Usa o triplet definido pelo perfil:
+#   -P glibc -> ADM_TARGET_TRIPLET=x86_64-lfs-linux-gnu (por exemplo)
+#   -P musl  -> ADM_TARGET_TRIPLET=x86_64-linux-musl
 #
-# Se um dia precisar de build custom, você pode:
-#   - usar hooks (pre_build, pre_install, post_install), ou
-#   - estender o adm.sh para suportar uma função tipo PKG_BUILD_FUNC.
+# Se PKG_TARGET_TRIPLET for exportado antes da chamada, ele prevalece.
+
+PKG_TARGET_TRIPLET="${PKG_TARGET_TRIPLET:-${ADM_TARGET_TRIPLET}}"
+
+###############################################################################
+# Opções de configure / build / install
+###############################################################################
+# O adm.sh monta algo como:
+#
+#   ./configure \
+#     --host="${TARGET_TRIPLET}" \
+#     --build="${TARGET_TRIPLET}" \
+#     --prefix=/usr \
+#     --sysconfdir=/etc \
+#     --localstatedir=/var \
+#     "${PKG_CONFIGURE_OPTS[@]}"
+#
+# Para o M4 final, podemos usar um configure bem simples. Se quiser tunar
+# (ex.: desativar NLS), use PKG_CONFIGURE_OPTS.
+
+PKG_CONFIGURE_OPTS=(
+  # Exemplo: desativar NLS para reduzir dependências
+  "--disable-nls"
+)
+
+# BUILD:
+#   O adm.sh fará: make -j"${ADM_JOBS:-$(nproc)}" "${PKG_MAKE_OPTS[@]}"
+#   No M4 não precisamos de nada especial.
+# PKG_MAKE_OPTS=()
+
+# INSTALL:
+#   O adm.sh fará: make DESTDIR="${destdir}" install "${PKG_MAKE_INSTALL_OPTS[@]}"
+#   Também não precisamos de ajustes especiais aqui.
+# PKG_MAKE_INSTALL_OPTS=()
