914bcc21d1ad3e5247656c70e71755f7bb812783
fcanatta5 <fernandocanatta5@gmail.com>
2025-12-16 19:08:36 -0300

    Update build.sh

--- script9/ports/base/dhcpcd/build.sh
+++ script9/ports/base/dhcpcd/build.sh
@@ -1,42 +1,23 @@
 #!/bin/sh
-set -e
+set -eu
 
-. /usr/share/adm/helpers.sh
-adm_defaults
-
-name="dhcpcd"
-version="10.2.4"
-release="1"
-
-srcdir_name="dhcpcd-10.2.4"
-source_urls="
-https://github.com/NetworkConfiguration/dhcpcd/releases/download/v10.2.4/dhcpcd-10.2.4.tar.xz
-"
+NAME=dhcpcd
+VERSION=10.2.4
+SOURCE="https://github.com/NetworkConfiguration/dhcpcd/releases/download/v${VERSION}/dhcpcd-${VERSION}.tar.xz"
 
 depends="core/make core/gcc core/pkgconf"
-makedepends=""
-
-prepare(){ :; }
 
 build() {
-  enter_srcdir_auto
-
-  # dhcpcd usa ./configure próprio
+  # layout padrão e previsível
   ./configure \
     --prefix=/usr \
     --sysconfdir=/etc \
     --dbdir=/var/lib/dhcpcd \
     --libexecdir=/usr/lib/dhcpcd
-
-  do_make
+  make -j"${JOBS:-$(nproc)}"
 }
 
-package() {
-  enter_srcdir_auto
-  make -j1 DESTDIR="$DESTDIR" install
-
-  mkdir -p "$DESTDIR/var/lib/dhcpcd"
-  ensure_destdir_nonempty
+install() {
+  make DESTDIR="${PKG}" install
+  install -d "${PKG}/var/lib/dhcpcd"
 }
-
-post_install(){ :; }
