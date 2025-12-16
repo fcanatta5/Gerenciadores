b8d33f85a40fdbb92ce11558a253b4a6a3636095
fcanatta5 <fernandocanatta5@gmail.com>
2025-12-16 17:06:35 -0300

    Update build.sh

--- script9/ports/base/vim/build.sh
+++ script9/ports/base/vim/build.sh
@@ -6,7 +6,7 @@ adm_defaults
 
 name="vim"
 version="9.1.1806"
-release="1"
+release="2"
 
 srcdir_name="vim-9.1.1806"
 
@@ -14,36 +14,58 @@ source_urls="
 https://ftp2.osuosl.org/pub/blfs/conglomeration/vim/vim-9.1.1806.tar.gz
 "
 
+# Para GUI/clipboard completo em X11/Wayland via GTK3:
+# - gtk3 puxa todo stack (glib/cairo/pango/etc.)
+# - xorg libs para clipboard e integração X11
+# - ncurses para o vim terminal
 depends="
 core/make
-core/ncurses
 core/pkgconf
+core/ncurses
+core/python
+core/perl
+core/gtk3
+core/xorgproto
+core/libxcb
 "
 
 makedepends=""
 
-prepare() { :; }
+prepare() {
+  :
+}
 
 build() {
   enter_srcdir_auto
   cd src
 
-  # Terminal-only (sem GUI/X), estável para musl
-  # multibyte ligado (útil mesmo sem glibc locales)
+  # GUI GTK3:
+  # - --enable-gui=gtk3: gvim com GTK3 (Wayland/X11 via backend do GTK)
+  # - --with-x: habilita recursos X (inclui +clipboard via X11)
+  # - --enable-clipboard: força suporte a clipboard
+  # - --with-features=huge: build completo
+  # - --enable-multibyte: UTF-8 útil mesmo em musl
+  # - --disable-nls: evita gettext/NLS
+  # - --enable-fail-if-missing: falha cedo se deps não existirem (melhor que “meio instalado”)
+  #
+  # Nota: removi --with-tlib=ncursesw porque em build GUI isso pode variar; pkg-config resolve.
   do_configure \
     --prefix=/usr \
     --with-features=huge \
     --enable-multibyte \
-    --with-tlib=ncursesw \
-    --disable-nls \
-    --disable-gui \
-    --without-x \
-    --disable-netbeans \
     --enable-terminal \
+    --enable-gui=gtk3 \
+    --with-x \
+    --enable-clipboard \
+    --disable-nls \
+    --enable-fail-if-missing \
     --with-vim-name=vim \
     --with-ex-name=ex \
     --with-view-name=view \
-    --with-global-runtime=/usr/share/vim
+    --with-global-runtime=/usr/share/vim \
+    --enable-python3interp=yes \
+    --enable-perlinterp=yes \
+    --disable-netbeans
 
   do_make
 }
@@ -54,10 +76,21 @@ package() {
 
   make -j1 DESTDIR="$DESTDIR" install
 
-  # symlinks vi (muito software assume)
+  # Symlinks tradicionais
   mkdir -p "$DESTDIR/usr/bin"
-  ln -sf vim "$DESTDIR/usr/bin/vi"
-  ln -sf vim "$DESTDIR/usr/bin/vimdiff" 2>/dev/null || true
+  ln -sf vim  "$DESTDIR/usr/bin/vi"
+  ln -sf gvim "$DESTDIR/usr/bin/gview" 2>/dev/null || true
+
+  # Desktop integration (opcional)
+  if [ -f "$FILESDIR/gvim.desktop" ]; then
+    install -Dm644 "$FILESDIR/gvim.desktop" \
+      "$DESTDIR/usr/share/applications/gvim.desktop"
+  fi
+
+  if [ -f "$FILESDIR/gvim.png" ]; then
+    install -Dm644 "$FILESDIR/gvim.png" \
+      "$DESTDIR/usr/share/icons/hicolor/48x48/apps/gvim.png"
+  fi
 
   ensure_destdir_nonempty
 }
