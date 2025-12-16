1f97e164b72bb3c1617e32e0a2c738bf0a7d1011
fcanatta5 <fernandocanatta5@gmail.com>
2025-12-16 09:05:29 -0300

    Update build.sh

--- script9/ports/core/zlib/build.sh
+++ script9/ports/core/zlib/build.sh
@@ -8,22 +8,42 @@ name="zlib"
 version="1.3.1"
 release="1"
 
-sources="
+# O tarball do zlib extrai para zlib-<version>
+srcdir_name="zlib-1.3.1"
+
+source_urls="
 https://zlib.net/zlib-1.3.1.tar.xz
 "
 
 depends=""
 makedepends=""
 
-prepare() { :; }
+prepare() {
+  # zlib não usa autotools
+  :
+}
 
 build() {
-  enter_srcdir
-  ./configure --prefix=/usr
+  enter_srcdir_auto
+
+  # zlib usa configure próprio
+  CHOST="$(gcc -dumpmachine 2>/dev/null || echo x86_64-linux-gnu)"
+
+  ./configure \
+    --prefix=/usr
+
   do_make
 }
 
 package() {
-  enter_srcdir
+  enter_srcdir_auto
+
   make DESTDIR="$DESTDIR" install
+
+  # zlib instala libz.so corretamente em /usr/lib
+  ensure_destdir_nonempty
+}
+
+post_install() {
+  : # nenhuma ação necessária
 }
